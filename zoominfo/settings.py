53c6e6a1eee4deed82aada3e740896ce2640497c
delta-x-dev <torre.delta.x@gmail.com>
2023-05-14 13:21:22 -0700

    Update settings.py
    Settings updated to delay downloads, reduce concurrent requests, and allow for cookies. Scraping the site this way returns less 429 errors.

--- zoominfo/settings.py
+++ zoominfo/settings.py
@@ -21,26 +21,27 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS=8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.2
+#before change: 0.2
+DOWNLOAD_DELAY = 20.0
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
+ #  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+ #  'Accept-Language': 'en',
 #}
 
 # Enable or disable spider middlewares
@@ -52,25 +53,28 @@ DOWNLOAD_DELAY = 0.2
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'zoominfo.middlewares.ZoominfoDownloaderMiddleware': 543,
+ #   'zoominfo.middlewares.ZoominfoDownloaderMiddleware': 543,
 #}
 
-# DOWNLOADER_MIDDLEWARES = {
-#     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 1
-# }
-
-ROTATING_PROXY_LIST_PATH = 'proxies.txt'
-NUMBER_OF_PROXIES_TO_FETCH = 10
-
 DOWNLOADER_MIDDLEWARES = {
-    'rotating_free_proxies.middlewares.RotatingProxyMiddleware': 610,
-    'rotating_free_proxies.middlewares.BanDetectionMiddleware': 620,
-}
+     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 1
+     }
+ 
+ROTATING_PROXY_LIST_PATH = 'proxies.txt'
+NUMBER_OF_PROXIES_TO_FETCH = 100
+
+#DOWNLOADER_MIDDLEWARES = {
+ #   'rotating_free_proxies.middlewares.RotatingProxyMiddleware': 610,
+ #   'rotating_free_proxies.middlewares.BanDetectionMiddleware': 620,
+#}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
+#TELNETCONSOLE_ENABLED = True
+#CLOSESPIDER_PAGECOUNT = 20
 #EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
+    #'scrapy.extensions.telnet.TelnetConsole': None,
+    #'scrapy.extensions.closespider.CloseSpider'
 #}
 
 # Configure item pipelines
@@ -81,16 +85,16 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 20
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 20
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
