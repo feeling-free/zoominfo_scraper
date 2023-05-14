d68a170c3b1ff5e41d876f5b4f1019d0e04aeaff
delta-x-dev <torre.delta.x@gmail.com>
2023-05-14 13:23:10 -0700

    Update zoominfo_scraper.py
    Commit updates the xpath to the descendant of the corresponding class and return text. Less blank returns this way.

--- zoominfo/spiders/zoominfo_scraper.py
+++ zoominfo/spiders/zoominfo_scraper.py
@@ -21,30 +21,8 @@ class ZoominfoSpider(scrapy.Spider):
             'company': kwargs['company'],
             'headquarters': response.xpath("//*[@class='vertical-gap first']/descendant::div/span/text()").getall(),
               'phone': response.xpath("//*[@class='vertical-gap']/descendant::span/text()").getall(),
-            'revenue': response.xpath("//h3[text()='Revenue']/descendant::span/text()").getall(),
+            'revenue': response.xpath("//*[@class='vertical-gap']/descendant::span/text()").getall(),
             'employees_num': response.xpath("//h3[text()='Employees']/following-sibling::div[1]/span/text()").getall(),
             'website': response.xpath("//*[@class='vertical-gap website-link']/descendant::a/text()").getall(),
-            'industry': response.xpath("//*[@class='company-chips-wrapper']/descendant::div/a/text()").getall()
+            'industry': response.xpath("//div[@class='company-chips-wrapper']/descendant-or-self::div/a/text()").getall()
         }
-#headquarters parent
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[1]/div/div[2]
-#headquarters text
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[1]/div/div[2]/span
-#phone number parent
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[2]
-#phone number text
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[2]/div/div[2]/span
-#website parent
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[3]
-#website text
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[3]/div/a
-#Revenue parent
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[4]
-#Revenue text
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/app-icon-text[4]/div/div[2]/span
-#Industry Wrappers
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/div[1]
-#Industry Text
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/div[1]/app-chips[1]/div/a
-#//*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/div[1]/app-chips[2]/div/a
-#/*[@id="left-container"]/div[1]/app-company-overview/div/div/div/div[1]/div[1]/app-chips[3]/div/a
